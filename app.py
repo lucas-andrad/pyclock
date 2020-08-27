58202eb8db4cadf855d92b8d5b90f169d9f964fb
Lucas Andrade <lafdesouza2002@outlook.com>
2020-08-26 21:14:06 -0300

    fixing a bug

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import tkinter
-from tkinter.ttk import *
 from time import strftime
+from tkinter.ttk import *
+
 
 root = tkinter.Tk()
 root.title("PyClock")
@@ -8,8 +9,11 @@ root.title("PyClock")
 def clock():
     hour = strftime("%H:%M:%S")
     label.config(text=hour)
-    label.after(1000, hour)
+    label.after(1000, clock)
 
 label = Label(root, font=("Helvetica", 60), background="#000", foreground="#00FF04")
 label.pack(anchor="center")
 
+clock()
+
+tkinter.mainloop()
